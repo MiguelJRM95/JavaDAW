f7472f1670746b2c1732916a53e6cac7c8f25664
MiguelJRM95 <miguelitoworkout@gmail.com>
2021-02-16 10:42:02 +0100

    Completing test Ejercicio12

--- Coleccion7/test/coleccion7/Ejercicio12Test.java
+++ Coleccion7/test/coleccion7/Ejercicio12Test.java
@@ -1,9 +1,9 @@
 package coleccion7;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 class Ejercicio12Test {
@@ -12,6 +12,12 @@ class Ejercicio12Test {
 	void testParametrosValidosYOscura() {
 		int[][] matriz = {{0,0,50,50,100,100},{0,0,50,50,100,100},{0,0,50,50,100,100},{150,150,150,200,200,200},{150,150,150,200,200,200},{150,150,150,200,200,200}};
 		assertTrue(Ejercicio12.esOscura(matriz, 200, 50));
+	}
+	
+	@Test
+	void testParametrosValidosYNoOscura() {
+		int[][] matriz = {{0,0,50,50,100,100},{0,0,50,50,100,100},{0,0,50,50,100,100},{150,150,150,200,200,200},{150,150,150,200,200,200},{150,150,150,200,200,200}};
+		assertFalse(Ejercicio12.esOscura(matriz, 200, 90));
 	}
 	
 	@Test
